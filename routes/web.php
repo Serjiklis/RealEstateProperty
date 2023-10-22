816fae29e119d32f72b0f4df9eddca43ad474458
serhii <tnmbel@gmail.com>
2023-10-22 20:22:33 +0100

    page 404

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Backend\RoleController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('main');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
